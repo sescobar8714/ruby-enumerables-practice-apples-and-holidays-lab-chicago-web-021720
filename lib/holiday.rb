48f1255571764c17bef00dd10f8578ebeec4a4cc
Sam Escobar <sescobar8714@gmail.com>
2020-03-11 13:22:03 -0700

    Done.

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -25,6 +25,8 @@ def add_supply_to_winter_holidays(holiday_hash, supply)
   # holiday_hash is identical to the one above
   # add the second argument, which is a supply, to BOTH the
   # Christmas AND the New Year's arrays
+  holiday_hash[:winter][:christmas] = supply
+  holiday_hash[:winter][:new_years] = supply
 
 end
 
@@ -32,19 +34,34 @@ end
 def add_supply_to_memorial_day(holiday_hash, supply)
   # again, holiday_hash is the same as the ones above
   # add the second argument to the memorial day array
-
+  holiday_hash[:spring][:memorial_day] = supply
 end
 
 def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
   # code here
   # remember to return the updated hash
+#season is fall 
+#columbus_day is the holiday 
+# supply _array = flags, parade floats, italian food ; strings 
+# add columbus day into season 
+
+
+  #binding.pry
+  holiday_hash[season][holiday_name] = supply_array 
+
 
 end
 
 def all_winter_holiday_supplies(holiday_hash)
   # return an array of all of the supplies that are used in the winter season
+  winter_holidays = holiday_hash[:winter]
+  winter_holidays.values.flatten 
 
-end
+end 
+
+
+
+
 
 def all_supplies_in_holidays(holiday_hash)
   # iterate through holiday_hash and print items such that your readout resembles:
@@ -53,14 +70,24 @@ def all_supplies_in_holidays(holiday_hash)
   #   New Years: Party Hats
   # Summer:
   #   Fourth Of July: Fireworks, BBQ
-  # etc.
-
+  # etc
+  holiday_hash.each do |season, holidays|
+    puts "#{season.capitalize}:"
+    holidays.each do |holiday, supplies|
+      puts"  #{holiday.to_s.split('_').map {|w| w.capitalize }.join(' ') }: #{supplies.join(", ")}"
+    end
+  end
 end
 
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
-
+  holiday_hash.map do |season, holidays|
+    holidays.map do |holiday, supplies|
+      holiday if supplies.include?("BBQ")
+    end
+  end.flatten.compact
+  #binding.pry
 end
 
 
